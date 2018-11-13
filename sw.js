bcec025d15234e18dd211d38cce07bb10868c295
DenGodovanets <40233692+DenGodovanets@users.noreply.github.com>
2018-11-13 18:28:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,30 @@
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open('randomquote').then(function (cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/src/main.js'                
-      ]);
-    })
-  );
-});
+// self.addEventListener('install', function (e) {
+//   e.waitUntil(
+//     caches.open('randomquote').then(function (cache) {
+//       return cache.addAll([
+//         '/',
+//         '/index.html',
+//         '/src/main.js'                
+//       ]);
+//     })
+//   );
+// });
 
-self.addEventListener('fetch', function (event) {
-  const url = event.request.url;
-  if (url.startsWith('https://andruxnet-random-famous-quotes.p.mashape.com/') || 
-      url.startsWith('http://quotes.rest/qod')) {
-    if (!navigator.onLine){
-      event.respondWith(new Response({
-        status: 200
-      }));
-      return; 
-    }
-  }
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
-});
+// self.addEventListener('fetch', function (event) {
+//   const url = event.request.url;
+//   if (url.startsWith('https://andruxnet-random-famous-quotes.p.mashape.com/') || 
+//       url.startsWith('http://quotes.rest/qod')) {
+//     if (!navigator.onLine){
+//       event.respondWith(new Response({
+//         status: 200
+//       }));
+//       return; 
+//     }
+//   }
+//   event.respondWith(
+//     caches.match(event.request).then(function (response) {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
 
