84eb17215b95e57b2805144e547d128404e14b5a
DenGodovanets <40233692+DenGodovanets@users.noreply.github.com>
2018-11-13 19:00:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,3 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/ServiceWorker.js')
-  .then(function(response) {
-    
-    // Service worker registration done
-    console.log('Registration Successful', response);
-  }, function(error) {
-    // Service worker registration failed
-    console.log('Registration Failed', error);
-  }
 
 self.addEventListener('install', function (e) {
   e.waitUntil(
